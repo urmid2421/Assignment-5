db441865a563effc8674a45b5e4ecce2f7ce72d3
urmid2421 <94037896+urmid2421@users.noreply.github.com>
2021-12-15 17:06:35 +0530

    Add files via upload

---
+++ Assignment 5.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+class power:
+
+    def __init__(x,y):
+        return (x**y)
+
+print(power.__init__(10,2))
+print(power.__init__(15,2))
+print(power.__init__(3,3))
+print(power.__init__(4,3))
+
